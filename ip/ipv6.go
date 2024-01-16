3a696f358e8abab97ca898aa1f125921975afb51
Caleb <netrunner@localhost.localdomain>
2024-01-16 22:35:44 +0300

    Implemented ipv6 address shortening, expansion, validation and conversion from hexadecimal  to binary format

--- ip/ipv6.go
+++ ip/ipv6.go
@@ -7,72 +7,240 @@ import (
 	"strings"
 )
 
-/*
-Splits an IPv6 address into its 8 colon-separated
-groups of 16bit segments called hextets
-
-For example, the IPv6 address 2001:0db8:3333:4444:5555:6666:7777:8888
-
-Will be converted to:
-
-	[]string{
-		'2001', '0db8', '3333', '4444', '5555', '6666', '7777',
-		'8888'
-	}
-*/
-func splitIPv6IntoSegments(ipv6 string) ([]string, error) {
-	numSegments := 8
-	ipv6 = strings.ReplaceAll(strings.Trim(ipv6, " "), ":", " ")
-	segments := strings.Fields(ipv6)
-
-	if len(segments) != numSegments {
-		return nil, fmt.Errorf("Expected %v segments from ipv6 address, but got %v instead\n", numSegments, len(segments))
-	}
-
-	return segments, nil
-}
-
-/*
-Checks if an address is a valid IPv6 address
-*/
-func isValidIPv6(ipv6 string) (bool, error) {
-	segments, err := splitIPv6IntoSegments(ipv6)
-	if err != nil {
-		return false, err
-	}
-
-	// Check if each segment is a valid hexadecimal value
-	for _, segment := range segments {
-		hexPattern := regexp.MustCompile("^[0-9a-fA-F]+$")
-		if !hexPattern.MatchString(segment) {
-			return false, fmt.Errorf("Segment %v of ipv6 address is not a valid hexadecimal value\n", segment)
-		}
-	}
-
-	return true, nil
-}
-
-func ConvertIpv6ToBinary(ipv6 string) (string, error) {
-	if ok, err := isValidIPv6(ipv6); !ok {
-		return "", err
-	}
-
-	segments, err := splitIPv6IntoSegments(ipv6)
+func IPv6StringToBinary(ipv6 string) (string, error) {
+	ipv6, err := validateIPv6Address(ipv6)
 	if err != nil {
 		return "", err
 	}
 
-	ipv6BinaryStr := ""
-
+	var ipv6Binary []string
+	segments := strings.Split(ipv6, ":")
 	for _, segment := range segments {
-		// Convert each segment to binary and append to binary string
+		// convert hexadecimal to binary/bytes
 		hexInt, ok := new(big.Int).SetString(segment, 16)
 		if !ok {
-			return "", fmt.Errorf("Invalid hexadecimal string: %s\n", segment)
+			return "", fmt.Errorf("invalid hexadecimal string %v detected in address %v", segment, ipv6)
 		}
 
-		ipv6BinaryStr += fmt.Sprintf("%016b", hexInt) + ":"
+		segment = fmt.Sprintf("%016b", hexInt.Int64())
+		ipv6Binary = append(ipv6Binary, segment)
 	}
 
-	return ipv6BinaryStr, nil
+	return strings.Join(ipv6Binary, ":"), nil
+}
+
+/*
+Shortens an IPv6 address.
+First, by removing all leading zeros in each hextet.
+The address:
+
+	fd00:4700:0010:0000:0000:0000:6814:d103
+
+will be shortened to:
+
+	fd00:4700:10:0:0:0:6814:d103.
+
+Second, replace the largest or leftmost group of consecutive, zero-value
+hextets with double colons, producing the shorter
+
+	fd00:4700:10::6814:d103.
+
+If your address has more than one group of consecutive zero-value hextets with
+the same size, you can only replace the leftmost zero group
+*/
+func ShortenIPv6Address(ipv6 string) string {
+	var segments []string
+
+	for _, segment := range strings.Split(ipv6, ":") {
+		stripped := stripLeadingZerosFromSegment(segment)
+		segments = append(segments, stripped)
+	}
+
+	ipv6 = strings.Join(segments, ":")
+	startIndex, stopIndex := indexOfIPv6ZeroGroup(ipv6)
+	if startIndex == -1 || stopIndex == -1 {
+		return ipv6
+	}
+
+	left := strings.Join(segments[:startIndex], ":")
+	right := strings.Join(segments[stopIndex+1:], ":")
+
+	return left + "::" + right
+}
+
+func ExpandIPv6Address(ipv6 string) string {
+	var segments []string
+
+	left, right, found := strings.Cut(ipv6, "::")
+	if found {
+		leftSegments := strings.Split(strings.Trim(left, " "), ":")
+		rightSegments := strings.Split(right, ":")
+
+		segments = append(segments, leftSegments...)
+
+		// number of zero groups between shortened ipv6 double colons ::
+		numSegments := 8
+		numZeroGroups := numSegments - (len(leftSegments) + len(rightSegments))
+
+		for i := 0; i < numZeroGroups; i++ {
+			segments = append(segments, "0")
+		}
+
+		segments = append(segments, rightSegments...)
+	} else {
+		segments = strings.Split(ipv6, ":")
+	}
+
+	// add leading zeros to each segment till 4 digit hexadecimal value
+	for i, segment := range segments {
+		str := fmt.Sprintf("%04v", segment)
+		segments[i] = str
+	}
+	return strings.Join(segments, ":")
+}
+
+func stripLeadingZerosFromSegment(str string) string {
+	result := str
+
+	for i := 0; i < len(str); i++ {
+		if len(result) == 1 {
+			return result
+		}
+
+		if result[0] == '0' {
+			result = result[i+1:]
+		} else {
+			return result
+		}
+	}
+
+	return result
+}
+
+/*
+Finds the index of the leftmost consecutive zero group existing within an IPv6 address.
+Returns -1, -1 if there are no zero groups or if existing
+zero groups are not consecutive
+
+For example:
+
+"fd12:3456:789a:0001:0000:0000:0000:0001"
+returns 4, 6
+
+"abcd:ef01:2345:6789:abcd:ef01:2345:6789"
+returns -1, -1 as there are no zero groups
+
+"2001:0db8:0000:0042:0000:8a2e:0370:7334"
+returns -1, -1 as the zero groups at index 2 and 4 are not consecutive; they exist on their own
+*/
+func indexOfIPv6ZeroGroup(ipv6 string) (int, int) {
+	ipv6 = strings.Trim(ipv6, " ")
+	segments := strings.Split(ipv6, ":")
+
+	startIndex := -1
+	stopIndex := -1
+	type zeroGroup struct {
+		startIndex int
+		stopIndex  int
+		length     int
+	}
+	groups := make([]zeroGroup, 0)
+
+	/*
+		Check is segment contains group of zeros i.e 0, 00, 000 or 0000
+		Explanation:
+
+		^: Asserts the start of the string.
+		0{1,4}: Matches 1 to 4 consecutive zeros at the beginning of the string.
+		$: Asserts the end of the string.
+	*/
+	zeroGroupPattern := regexp.MustCompile("^0{1,4}$")
+
+	for index, segment := range segments {
+		isZeroGroup := zeroGroupPattern.MatchString(segment)
+		isConsecutive := startIndex != stopIndex
+
+		if isZeroGroup {
+			if startIndex == -1 {
+				startIndex = index
+				stopIndex = index
+			} else {
+				stopIndex++
+			}
+
+			onLastIndex := index == len(segments)-1
+			if onLastIndex && isConsecutive {
+				groups = append(groups, zeroGroup{
+					startIndex: startIndex,
+					stopIndex:  stopIndex,
+					length:     (stopIndex - startIndex) + 1,
+				})
+			}
+
+		} else {
+			// zero group was already found but now terminating
+			// as current segment not zero group
+			if startIndex != -1 {
+				if isConsecutive {
+					groups = append(groups, zeroGroup{
+						startIndex: startIndex,
+						stopIndex:  stopIndex,
+						length:     (stopIndex - startIndex) + 1,
+					})
+				}
+
+				// reset
+				startIndex = -1
+				stopIndex = -1
+			}
+
+		}
+	}
+
+	if len(groups) == 0 {
+		return -1, -1
+	} else {
+		// Find largest or leftmost consecutive zero group
+		largestGroup := groups[0]
+		for _, group := range groups {
+			if group.length > largestGroup.length {
+				largestGroup = group
+			}
+		}
+		return largestGroup.startIndex, largestGroup.stopIndex
+	}
+}
+
+func validateIPv6Address(ipv6 string) (string, error) {
+	// strip zone identifier (like %eth0) on IPv6 address, if any
+	ipv6, _, _ = strings.Cut(ipv6, "%")
+
+	isShortened := strings.Contains(ipv6, "::")
+	if isShortened {
+		count := strings.Count(ipv6, "::")
+		if count > 1 {
+			return "", fmt.Errorf("multiple double colons found in shortened IPv6 address. Expected only 1")
+		}
+	}
+
+	ipv6 = ExpandIPv6Address(ipv6)
+	segments := strings.Split(ipv6, ":")
+	if len(segments) != 8 {
+		return "", fmt.Errorf("invalid number of hextets. Found %v expected %v", len(segments), 8)
+	}
+
+	hexPattern := regexp.MustCompile("^[0-9a-fA-F]+$")
+
+	for _, segment := range segments {
+		isValidHex := hexPattern.MatchString(segment)
+		if !isValidHex {
+			return "", fmt.Errorf("ipv6 address contains non-hexadecimal hextext %v", segment)
+		}
+
+		if len(segment) != 4 {
+			return "", fmt.Errorf("expected 4 digit hextet string but found length of %v instead", len(segment))
+		}
+	}
+
+	return ipv6, nil
 }
--- ip/ipv6_test.go
+++ ip/ipv6_test.go
@@ -1,7 +1,151 @@
 package ip
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSplitIPv6IntoSegments(t *testing.T) {
+func TestIPv6StringToBinary(t *testing.T) {
+	tests := []struct {
+		address    string
+		expected   string
+		shouldFail bool
+	}{
+		{"2001:0db8:85a3:0000:0000:8a2e:0370:7334", "0010000000000001:0000110110111000:1000010110100011:0000000000000000:0000000000000000:1000101000101110:0000001101110000:0111001100110100", false}, // Valid IPv6 address
+		{"::1", "0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000001", false},                                     // Valid IPv6 loopback address
+		{"fe80::1%eth0", "1111111010000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000000:0000000000000001", false},                            // Valid IPv6 address with zone identifier
+		{"invalid", "", true},     // Invalid IPv6 address
+		{"192.168.0.1", "", true}, // Not an IPv6 address
+	}
 
+	for _, test := range tests {
+		t.Run(test.address, func(t *testing.T) {
+			got, err := IPv6StringToBinary(test.address)
+
+			if test.shouldFail {
+				if err == nil {
+					t.Errorf("expected an error for invalid address %v", test.address)
+				}
+
+			} else {
+				if err != nil {
+					t.Errorf("unexpected error %v while converting valid address %v", err, test.address)
+				}
+
+				if got != test.expected {
+					t.Errorf("expected binary ipv6 address %v but got %v instead", test.expected, string(got))
+				}
+			}
+		})
+	}
+}
+
+func TestShortenIPv6Address(t *testing.T) {
+	tests := []struct {
+		address  string
+		expected string
+	}{
+		{"2001:0db8:85a3:0000:0001:8a2e:0370:7334", "2001:db8:85a3:0:1:8a2e:370:7334"}, // Should remove leading zeros
+		{"2001:0000:0000:0001:0000:0000:0000:0000", "2001:0:0:1::"},                    // Multiple consecutive zero groups of different sizes; should compress largest consecutive zero group
+		{"fe80::1", "fe80::1"},
+		{"3ffe:1900:4545:3:200:f8ff:fe21:67cf", "3ffe:1900:4545:3:200:f8ff:fe21:67cf"},
+		{"::", "::"},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Shortening IPv6 address %v", test.address)
+
+		t.Run(testName, func(t *testing.T) {
+			got := ShortenIPv6Address(test.address)
+
+			if got != test.expected {
+				t.Errorf("expected shortened IPv6 address %v but got %v instead\n", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestExpandIPv6Address(t *testing.T) {
+	tests := []struct {
+		address  string
+		expected string
+	}{
+		{"2001:db8:85a3::8a2e:370:7334", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"},
+		{"fe80::1", "fe80:0000:0000:0000:0000:0000:0000:0001"},
+		{"3ffe:1900:4545:3:200:f8ff:fe21:67cf", "3ffe:1900:4545:0003:0200:f8ff:fe21:67cf"},
+		{"fd12:3456:789a:1::1", "fd12:3456:789a:0001:0000:0000:0000:0001"},
+		{"::", "0000:0000:0000:0000:0000:0000:0000:0000"},
+		{"2001:0:0:1::", "2001:0000:0000:0001:0000:0000:0000:0000"},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Expanding IPv6 address %v", test.address)
+
+		t.Run(testName, func(t *testing.T) {
+			got := ExpandIPv6Address(test.address)
+
+			if got != test.expected {
+				t.Errorf("expected expanded IPv6 address %v but got %v instead\n", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestStripLeadingZerosFromSegment(t *testing.T) {
+	tests := []struct {
+		str      string
+		expected string
+	}{
+		{"2001", "2001"},
+		{"0db8", "db8"},
+		{"0005", "5"},
+		{"0000", "0"},
+		{"0370", "370"},
+		{"0600", "600"},
+		{"ef01", "ef01"},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Stripping leading zeros from str %v", test.str)
+
+		t.Run(testName, func(t *testing.T) {
+			got := stripLeadingZerosFromSegment(test.str)
+
+			if got != test.expected {
+				t.Errorf("expected %v but got %v instead", test.expected, got)
+			}
+		})
+	}
+}
+
+func TestIndexOfIPv6ZeroGroup(t *testing.T) {
+	tests := []struct {
+		address    string
+		startIndex int
+		stopIndex  int
+	}{
+		{"2001:0db8:0000:0042:0000:8a2e:0370:7334", -1, -1},
+		{"fe80:0000:0000:0000:0000:0000:0000:0001", 1, 6},
+		{"3ffe:1900:0000:0003:0200:f8ff:fe21:67cf", -1, -1},
+		{"2001:db8:0000:0000:0000:0000:0000:0001", 2, 6},
+		{"fd12:3456:789a:0001:0000:0000:0000:0001", 4, 6},
+		{"abcd:ef01:2345:6789:abcd:ef01:2345:6789", -1, -1},
+		{"0000:0000:0000:0000:0000:0000:0000:0000", 0, 7},
+		{"2001:0000:0000:0001:0000:0000:0000:0000", 4, 7},
+		{"1234:0000:5678:0000:abcd:0000:ef01:0000", -1, -1},
+		{"::1", -1, -1},
+		{"2001:0:0:1::", 1, 2},
+	}
+
+	for _, test := range tests {
+		testName := fmt.Sprintf("Grouping zeros in IPv6 address %v", test.address)
+
+		t.Run(testName, func(t *testing.T) {
+			gotStartIndex, gotStopIndex := indexOfIPv6ZeroGroup(test.address)
+
+			if gotStartIndex != int(test.startIndex) || gotStopIndex != int(test.stopIndex) {
+				t.Errorf("expected grouping start and stop indexes of (%v, %v) but got (%v, %v) instead", test.startIndex, test.stopIndex, gotStartIndex, gotStopIndex)
+			}
+		})
+	}
 }
